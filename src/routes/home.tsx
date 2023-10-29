94050f55471930763a3ca62420e7ca66503fe1b9
chanho <dirtyface1234@naver.com>
2023-10-30 03:01:50 +0900

    10/30 새벽 3시

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,6 +1,6 @@
+
 import styled from "styled-components";
 import { auth } from "../firebase";
-import { Title } from "../components/auth-components";
 import { useNavigate } from "react-router-dom";
 
 export const Button = styled.button`
@@ -30,43 +30,6 @@ export const SvgWrapper = styled.svg`
   width: 50px;
   height: 50px;
 `;
-const MainSlider = styled.div`
-  display: flex;
-  overflow: hidden;
-  width: 100%;
-  height: 60px;
-`;
-
-const SliderImage = styled.div`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  background-color: blue;
-`;
-
-const KMOOCIntro = styled.div`
-  padding: 40px;
-  background-color: #eef2f7;
-`;
-
-const Sidebar = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-`;
-
-const SidebarLink = styled.a`
-  padding: 10px;
-  text-decoration: none;
-  color: #333;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  transition: background-color 0.2s;
-
-  &:hover {
-    background-color: #e0e0e0;
-  }
-`;
 
 
 export default function Home() {
@@ -78,20 +41,55 @@ export default function Home() {
     auth.signOut();
   };
   return (
-    <Container>
-      <Header><SvgWrapper fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"></path>
-</SvgWrapper><Title>DAO college</Title>
-<button onClick={logOut}>Log Out</button>
-</Header>
-      <MainSlider>
-        <SliderImage>
-          <Button onClick={()=>onClicked("/introduction")}>DAO college 소개</Button>
-        </SliderImage> 
-        <Button onClick={()=>onClicked("/governance-vote")}>개설 강의 제안</Button>
-          <Button onClick={()=>onClicked("/sugang")}>수강신청</Button>
-          <Button onClick={()=>onClicked("/profile")}>마이 페이지</Button>
-      </MainSlider>
-    </Container>
+    <div className="font-sans w-full h-full bg-white text-black">
+      <div className="flex items-start justify-start p-5 bg-white border-b">
+        <svg 
+          className="w-12 h-12 fill-current text-white"
+          fill="none" 
+          stroke="black" 
+          strokeWidth="1.5" 
+          viewBox="0 0 24 24" 
+          xmlns="http://www.w3.org/2000/svg" 
+          aria-hidden="true"
+        >
+          <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"></path>
+        </svg>
+        <div className="ml-4 text-3xl font-bold">DAO college</div>
+        <button 
+          className="ml-auto px-4 py-2 bg-gray-800 text-white rounded-md hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
+          onClick={logOut}
+        >
+          Log Out
+        </button>
+      </div>
+      <div className=" bg-blue-400 flex overflow-hidden h-16 mt-5">
+        <div className="w-full h-full bg-cyan-600 flex items-center border-b">
+          <button 
+            className="flex items-center justify-start px-5 py-3 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
+            onClick={() => onClicked("/introduction")}
+          >
+            DAO college 소개
+          </button>
+        </div>
+        <button 
+          className="flex items-center justify-start px-5 py-3 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
+          onClick={() => onClicked("/governance-vote")}
+        >
+          개설 강의 제안
+        </button>
+        <button 
+          className="flex items-center justify-start px-5 py-3 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
+          onClick={() => onClicked("/sugang")}
+        >
+          수강신청
+        </button>
+        <button 
+          className="flex items-center justify-start px-5 py-3 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
+          onClick={() => onClicked("/profile")}
+        >
+          마이 페이지
+        </button>
+      </div>
+    </div>
   );
 }
--- src/routes/introduction.tsx
+++ src/routes/introduction.tsx
@@ -1,28 +1,32 @@
-import { styled } from "styled-components";
-import { Container, Header, SvgWrapper } from "./home";
-import { Title } from "../components/auth-components";
-
-const Wrapper = styled.div`
-  height: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Text = styled.span`
-  font-size: 24px;
-`;
+import "./index.css"
 
 export default function Introduction() {
   return (
-    <Container>
-        <Header><SvgWrapper fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"></path>
-  </SvgWrapper><Title>DAO college</Title></Header>
-        <Wrapper>
-      <Text>DAO college is started from blockchain college that called by "Blue Node" in INHA university </Text>
-    </Wrapper>
-    </Container>
-    
+    <div className="font-sans w-full h-full bg-white text-black flex flex-col">
+      <div className="flex items-start justify-start p-5 bg-white border-b">
+        <svg 
+          className="w-12 h-12 fill-current text-white"
+          fill="none" 
+          stroke="black" 
+          strokeWidth="1.5" 
+          viewBox="0 0 24 24" 
+          xmlns="http://www.w3.org/2000/svg" 
+          aria-hidden="true"
+        >
+          <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"></path>
+        </svg>
+        <div className="ml-4 text-3xl font-bold">DAO college</div>
+      </div>
+      <div className="flex-grow flex items-center justify-center p-10">
+        <div className="text-center">
+          <p>
+            DAO college는 혁신적인 교육 방식으로 블록체인 기술의 잠재력을 최대화하기 위한 플랫폼입니다.
+          </p>
+          <p className="mt-4">
+            인하대학교의 블록체인 학회 블루노드에서 시작되어, 글로벌 네트워크를 형성하며 세계적인 인정을 받고 있습니다.
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
